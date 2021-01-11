bd5fdd3674402ca01dfdf8fb2f3724826b08e5d8
bora <boraoren@gmail.com>
2021-01-11 15:31:05 +1100

    feat(location): added

--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import {ImageBackground, StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 import ButtonGroup from '../../components/button/group';
 import City from '../../components/text/city';
 import DateTime from '../../components/text/datetime';
+import Geolocation from '@react-native-community/geolocation';
 
 const imageBackgroundRooster = require('../../components/images/background/rooster.png');
 
 const MainScreen = () => {
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition((info) => setLocation(info));
+  }, []);
+
   return (
     <>
       <ImageBackground
         style={styles.imageBackground}
         source={imageBackgroundRooster}>
         <City />
+        <Text>{JSON.stringify(location)}</Text>
         <DateTime />
         <View style={styles.buttonGroupWrapper}>
           <ButtonGroup button1Title={'Snooze'} button2Title={'Dismiss'} />
